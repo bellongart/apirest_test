d68150534d24dae77dfd1b1eddd7b2a277d2f31f
homet <bellolongartj@gmail.com>
2024-07-22 20:31:36 -0400

    change localhost by 127.0.0.1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST      = process.env.DB_HOST || 'localhost'
+export const DB_HOST      = process.env.DB_HOST || '127.0.0.1'
 export const DB_USER      = process.env.DB_USER || 'guest'
 export const DB_PASSWORD  = process.env.DB_PASSWORD || '123456'
 export const DB_NAME      = process.env.DB_NAME || 'api_db'
